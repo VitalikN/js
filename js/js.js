e894e96ea24f1d986272e928fc8ebaa2a3307e8a
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-10-17 10:54:26 +0300

    навчання

---
+++ js/js.js
@@ -0,0 +1,7 @@
+const age = 5;
+const totalPrice = 200;
+const message = 'привіт';
+const userName = 'Vitalik';
+const isOpen = true;
+const shouldConfirm = false;
+console.log('ціна:', totalPrice);
