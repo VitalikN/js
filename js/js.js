d78a129e71e3290bf8b22b5b41fc1e1fad0ffb3b
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-10-20 12:08:59 +0300

    автопереівірка

--- js/js.js
+++ js/js.js
@@ -1,7 +1,24 @@
-const age = 5;
-const totalPrice = 200;
-const message = 'привіт';
-const userName = 'Vitalik';
-const isOpen = true;
-const shouldConfirm = false;
-console.log('ціна:', totalPrice);
+// const age = 5;
+// const totalPrice = 200;
+// const message = 'привіт';
+// const userName = 'Vitalik';
+// const isOpen = true;
+// const shouldConfirm = false;
+// console.log('ціна:', totalPrice);
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   const message =
+//     available >= ordered
+//       ? 'Not enough goods in stock!'
+//       : 'Order is processed, our manager will contact you.';
+
+//   // if (available >= ordered) {
+//   //    message = 'Not enough goods in stock!';
+//   //  } else {
+//   //    message = 'Order is processed, our manager will contact you.';
+//   //  }
+//   // Change code above this line
+//   return message;
+// }
