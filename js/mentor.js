2a5a1b0f6c3204a9a43058c6ef754cb51cdba0ef
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-11-16 09:44:40 +0200

    1

--- js/mentor.js
+++ js/mentor.js
@@ -144,35 +144,35 @@
 // Ви пропонуєте користувачу список доступних ресторанів для замовлення їжі.
 // Користувач обирає конкретний ресторан і отримує список доступних пунктів в меню для замовлення із вказанням їх вартості.
 // Користувач обирає своє замовлення, після чого має отримати його підтвердження та основну інформацію: "що замовив, вартість та час доставки".
-// const restaurants = [
-//   {
-//     order: [],
-//     brand: "KFC",
-//     menu: {
-//       chicken: 50,
-//       burger: 50,
-//     },
-//     deliveryTime: 60,
-//   },
-//   {
-//     order: [],
-//     brand: "mcDonalds",
-//     menu: {
-//       cola: 25,
-//       burger: 30,
-//     },
-//     deliveryTime: 30,
-//   },
-//   {
-//     order: [],
-//     brand: "Burger King",
-//     menu: {
-//       burgerXXL: 170,
-//       burger: 70,
-//     },
-//     deliveryTime: 20,
-//   },
-// ];
+const restaurants = [
+  {
+    order: [],
+    brand: 'KFC',
+    menu: {
+      chicken: 50,
+      burger: 50,
+    },
+    deliveryTime: 60,
+  },
+  {
+    order: [],
+    brand: 'mcDonalds',
+    menu: {
+      cola: 25,
+      burger: 30,
+    },
+    deliveryTime: 30,
+  },
+  {
+    order: [],
+    brand: 'Burger King',
+    menu: {
+      burgerXXL: 170,
+      burger: 70,
+    },
+    deliveryTime: 20,
+  },
+];
 // const services = {
 //   showMenu() {},
 //   getMenu() {},
@@ -181,7 +181,7 @@
 // };
 // const torpedaDelivery = {
 //   order: [],
-//   chosenRestaurant: "",
+//   chosenRestaurant: "", обрати ресторан
 //   getAvailableRestaurants() {},
 //   chooseRestaurant() {},
 //   chooseDishes() {},
